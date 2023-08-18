846e3540e61d76420fd1c9ee5447aead07972ba2
Gautam Kumar <gk1497singh@gmail.com>
2023-08-18 12:33:36 +0530

    DAA Exp2 code

--- Exp2.java
+++ Exp2.java
@@ -4,16 +4,14 @@ public class Exp2 {
         if (n == 0) {
             return 1;
         }
-
-        double temp = power(x, n / 2);
-
+        double halfPower = power(x,n/ 2);
         if (n % 2 == 0) {
-            return temp * temp;
+            return halfPower * halfPower;
         } else {
             if (n > 0) {
-                return x * temp * temp;
+                return x * halfPower * halfPower;
             } else {
-                return (temp * temp) / x;
+                return (halfPower * halfPower) / x;
             }
         }
     }
